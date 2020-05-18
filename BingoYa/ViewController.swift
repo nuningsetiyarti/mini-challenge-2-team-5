6bc2929f6d796571ebab55329a0713e69fa45662
Adrian Renaldi <renaldi.adrian@gmail.com>
2020-05-18 11:00:51 +0700

    Initial Push

---
+++ BingoYa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingoYa
+//
+//  Created by Adrian Renaldi on 18/05/20.
+//  Copyright © 2020 Adrian Renaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
